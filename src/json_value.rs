a2adb60ab58251695d09d0e7d0cd4cde9d5d8809
rhysd <lin90162@yahoo.co.jp>
2016-08-17 00:33:22 +0900

    access to JSON value with index operator

--- src/json_value.rs
+++ src/json_value.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::ops::Index;
 
 #[derive(Debug, Clone)]
 pub enum JsonValue {
@@ -10,3 +11,31 @@ pub enum JsonValue {
     Object(HashMap<String, JsonValue>),
 }
 
+impl<'a> Index<&'a str> for JsonValue {
+    type Output = JsonValue;
+
+    fn index<'b>(&'b self, key: &'a str) -> &'b JsonValue {
+        let obj = match self {
+            &JsonValue::Object(ref o) => o,
+            _ => panic!("Attempted to access an object with key '{}' but actually it was {:?}", key, self),
+        };
+
+        let val = obj.get(key);
+        match val {
+            Some(ref json) => json,
+            None => panic!("Key '{}' was not found in {:?}", key, self),
+        }
+    }
+}
+
+impl Index<String> for JsonValue {
+    type Output = JsonValue;
+
+    fn index<'a>(&'a self, key: String) -> &'a JsonValue {
+        // Note:
+        //   key   is 'String'
+        //   *key  is 'str'
+        //   &*key is '&str'
+        &self[&*key]
+    }
+}
