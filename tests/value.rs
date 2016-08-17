d5e9932139523d25c221cbecb51333caadfc6647
rhysd <lin90162@yahoo.co.jp>
2016-08-17 15:57:36 +0900

    add tests for operator == and !=

--- tests/value.rs
+++ tests/value.rs
@@ -13,12 +13,50 @@ const STR_OK: &'static str = r#"
           }
         "#;
 
-#[test]
-fn test_access_with_index_operator() {
-    let parsed = match parse_str(STR_OK) {
+fn must_parse(s: &'static str) -> JsonValue {
+    match parse_str(s) {
         Ok(j) => j,
         Err(e) => panic!("Parse failed: {:?}", e),
-    };
+    }
+}
+
+#[test]
+fn test_equality_property() {
+    let a = must_parse(STR_OK);
+    let b = must_parse(STR_OK);
+    let c = must_parse(STR_OK);
+    assert!(a == a);
+
+    assert!(a == b);
+    assert!(b == a);
+
+    assert!(b == c);
+    assert!(c == a);
+}
+
+#[test]
+fn test_not_equal() {
+    assert!(must_parse(r#"{}"#) != must_parse(r#"{"foo": 42}"#));
+    assert!(must_parse(r#"{"foo": true}"#) != must_parse(r#"{"foo": 42}"#));
+    assert!(must_parse(r#"{"foo": 21}"#) != must_parse(r#"{"foo": 42}"#));
+    assert!(must_parse(r#"{"bar": 42}"#) != must_parse(r#"{"foo": 42}"#));
+    assert!(must_parse(r#"{"arr": [1, 2, 3]}"#) != must_parse(r#"{"arr": [1, 2]}"#));
+    assert!(must_parse(r#"{"foo": null}"#) != must_parse(r#"{"foo": 42}"#));
+    assert!(must_parse(r#"{"foo": {"bar": 42}}"#) != must_parse(r#"{"foo": {"bar": 21}}"#));
+    assert!(must_parse(r#"{"foo": [{"bar": 42}]}"#) != must_parse(r#"{"foo": [{"baz": 42}]}"#));
+}
+
+#[test]
+fn test_equality_edge_cases() {
+    assert!(must_parse(r#"{}"#) == must_parse(r#"{}"#));
+    assert!(must_parse(r#"{"foo": []}"#) == must_parse(r#"{"foo": []}"#));
+    assert!(must_parse(r#"{"a": null}"#) != must_parse(r#"{}"#));
+    assert!(must_parse(r#"{"foo": [42]}"#) != must_parse(r#"{"foo": []}"#));
+}
+
+#[test]
+fn test_access_with_index_operator() {
+    let parsed = must_parse(STR_OK);
     // XXX: To be refactored
     match parsed["bool"] {
         JsonValue::Boolean(ref b) => assert!(b),
