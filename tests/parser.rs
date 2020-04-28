9aaa3b95a0845b611cfecab235391c696f6a1aab
rhysd <lin90162@yahoo.co.jp>
2020-04-28 23:14:27 +0900

    run failure case tests

--- tests/parser.rs
+++ tests/parser.rs
@@ -2,35 +2,17 @@ use tinyjson::*;
 
 use std::fs;
 use std::io::Read;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
-fn json_suite_paths() -> Vec<PathBuf> {
-    let assets_dir = "./tests/assets";
-    fs::read_dir(assets_dir)
+fn json_suite_paths() -> impl Iterator<Item = PathBuf> {
+    fs::read_dir("./tests/assets")
         .expect("'assets' directory not found")
         .map(|e| e.expect("Incorrect directory entry"))
         .filter(|e| e.file_type().expect("Failed to obtain file type").is_file())
-        .map(|e| Path::new(assets_dir).join(e.file_name()))
-        .collect()
+        .map(|e| e.path())
 }
 
-#[allow(dead_code)]
-fn each_fail_case<Callback>(cb: Callback)
-where
-    Callback: Fn(String) -> (),
-{
-    let paths = json_suite_paths();
-    let failed_cases = paths
-        .iter()
-        .filter(|p| p.to_str().unwrap().contains("fail"));
-    for failed in failed_cases {
-        let mut f = fs::File::open(failed.to_str().unwrap()).expect("Failed to open file");
-        let mut buf = String::new();
-        f.read_to_string(&mut buf).expect("Failed to read file");
-        cb(buf);
-    }
-}
-
+/*
 #[allow(dead_code)]
 fn each_pass_case<Callback>(cb: Callback)
 where
@@ -47,6 +29,7 @@ where
         cb(buf);
     }
 }
+*/
 
 const STR_OK: &'static str = r#"
           {
@@ -76,15 +59,33 @@ fn test_reference_lifetime() {
     assert!(f().is_ok());
 }
 
-/*
 #[test]
 fn test_parse_failed() {
-    each_fail_case(|json| {
-        let parsed = parse(&json);
-        assert!(parsed.is_err(), "Incorrectly parse succeeded: {:?}: {:?}", json, parsed);
-    });
+    for path in json_suite_paths() {
+        if !path
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .starts_with("fail")
+        {
+            continue;
+        }
+        let mut f = fs::File::open(&path).expect("Failed to open file");
+        let mut broken = String::new();
+        f.read_to_string(&mut broken).expect("Failed to read file");
+        let parsed: JsonParseResult = broken.parse();
+        assert!(
+            parsed.is_err(),
+            "Incorrectly parse succeeded {:?}: {:?}: {:?}",
+            path,
+            parsed,
+            broken,
+        );
+    }
 }
 
+/*
 #[test]
 fn test_parse_passed() {
     each_pass_case(|json| {
