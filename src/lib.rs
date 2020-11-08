3003bf81fa87ca51bf3cb8e66406f25bce5a8390
rhysd <lin90162@yahoo.co.jp>
2020-11-09 01:14:52 +0900

    fix clippy and rustfmt warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+// Suppress warning which prefers `matches!` macro to `match` statement since the macro was
+// introduced in recent Rust 1.42. This library should support older Rust.
+#![allow(clippy::match_like_matches_macro)]
+
 mod generator;
 mod json_value;
 mod parser;
 
 pub use generator::*;
-pub use json_value::{JsonValue, UnexpectedValue, InnerAsRef};
+pub use json_value::{InnerAsRef, JsonValue, UnexpectedValue};
 pub use parser::*;
