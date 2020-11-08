ff1c92800849e17aaab05e669d12367ee4655b43
Philipp Ludwig <git@philippludwig.net>
2020-11-08 14:00:47 +0100

    Satisfy clippy.

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@ mod json_value;
 mod parser;
 
 pub use generator::*;
-pub use json_value::{JsonValue, UnexpectedValue, InnerAsRef};
+pub use json_value::{InnerAsRef, JsonValue, UnexpectedValue};
 pub use parser::*;
