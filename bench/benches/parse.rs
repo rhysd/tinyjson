9663b957a541e49d57d4a9b44451533a327bc080
rhysd <lin90162@yahoo.co.jp>
2023-01-05 17:53:00 +0900

    add more practical benchmark data

--- bench/benches/parse.rs
+++ bench/benches/parse.rs
@@ -1,5 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use std::convert::TryInto;
 use tinyjson::JsonValue;
+use tinyjson_bench::load_my_2020_dec_tweets;
 
 fn parse(c: &mut Criterion) {
     c.bench_function("parse::string", |b| {
@@ -10,8 +12,18 @@ fn parse(c: &mut Criterion) {
     });
     c.bench_function("parse::number", |b| {
         b.iter(|| {
-            let value: JsonValue = r#"123.456e10"#.parse().unwrap();
-            assert!(matches!(value, JsonValue::Number(_)));
+            for s in [
+                "0",
+                "3141592",
+                "-3141592",
+                "314.1592",
+                "-314.1592",
+                "123.456e10",
+                "-123.456e-10",
+            ] {
+                let value: JsonValue = s.parse().unwrap();
+                assert!(matches!(value, JsonValue::Number(_)));
+            }
         });
     });
     c.bench_function("parse::bool", |b| {
@@ -31,6 +43,14 @@ fn parse(c: &mut Criterion) {
             let value: JsonValue =
                 r#"{"num":42,"bool":true,"array":[],"object":{}}"#.parse().unwrap();
             assert!(matches!(value, JsonValue::Object(_)));
+        });
+    });
+    c.bench_function("parse::my_tweets_2020_dec", |b| {
+        let s = load_my_2020_dec_tweets();
+        b.iter(|| {
+            let value: JsonValue = s.parse().unwrap();
+            let tweets: Vec<_> = value.try_into().unwrap();
+            assert_eq!(tweets.len(), 217);
         });
     });
 }
--- bench/src/lib.rs
+++ bench/src/lib.rs
@@ -1,1 +1,8 @@
+use std::fs;
+use std::path::PathBuf;
 
+pub fn load_my_2020_dec_tweets() -> String {
+    let mut p = PathBuf::from("assets");
+    p.push("tweets_2020_dec.json");
+    fs::read_to_string(p).unwrap()
+}
